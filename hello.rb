ea61d1fe6a3065b9ee5b5a2a4f034428ab4a7f3d
Nélia Khiter <nelia.khiter@live.fr>
2022-10-03 13:44:13 +0200

    first commit on rb

---
+++ hello.rb
@@ -0,0 +1,103 @@
+<<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="bootstrap.css" rel="stylesheet">
+
+    <title>The Surfing Project</title>
+  </head>
+
+  <body>
+      <div class="container-lg p-2">
+ <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="C:\Users\Home\Documents\THP\Jour 5 Site Groupe\img\logo.png" height="40%" width="40%"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <h2>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+      </h2>
+    </div>
+  </div>
+</nav>    
+
+<div class="px-4 py-5 my-5 text-center">
+    <img class="d-block mx-auto mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+    <h1 class="display-5 fw-bold">Centered hero</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+      </div>
+    </div>
+  </div>
+
+
+
+  <section>
+    
+
+    <h2 class="display-2 text-center" style="margin-top: 40px;">Notre équipe</h2>
+
+<div class="container my-container" style="margin-top: 50px;">
+  <div class="row text-center">
+
+    <div class="col-lg-4 mb-4 my-col">
+      <div class="card" style="width: 18rem;">  
+  <img src="mono.jpg" class="mono" alt="Ski instructor">
+  <div class="card-body">
+    <p class="card-text">Samy, notre moniteur de ski depuis déjà 25 ans est un vrai pilier du camp. Les conditions météoroligiques et la basse altitude ne lui ont pas encore permis de vous faire passer votre première étoile, mais comptez sur lui pour animer les soirées sur la plage .</p>
+  </div>
+</div>
+      
+    </div>
+    <div class="col-lg-4 mb-4 my-col"> 
+
+<div class="card" style="width: 18rem;">
+  <img src="profsurf.jpg" class="profsurf" alt="Surf instructor">
+  <div class="card-body">
+    <p class="card-text">Jeff sera votre moniteur de surf tout au long de votre séjour, il vous apprendra les rudiments de cette discipline avec patience mais pas trop.  </p>
+  </div>
+</div>
+    </div>
+    
+ </div>
+
+   <div class="col-lg-4 mb-4 my-col"> 
+
+<div class="card" style="width: 18rem;">
+  <img src="yoga.jpg" class="yoga" alt="Yoga Instructor">
+  <div class="card-body">
+    <p class="card-text">Sandy sera votre coach Yoggy, ce rendez-vous chaque matin sur la plage pour une séance de Yoga est un indispensable avant d'aller affronter les vagues</p>
+  </div>
+</div>
+    </div>
+    
+ </div> 
+
+  </section>
+
+</body>
